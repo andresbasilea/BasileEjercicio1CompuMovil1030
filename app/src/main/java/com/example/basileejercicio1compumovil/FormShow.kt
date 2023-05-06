64fb9f66f6d7b132e1b19663a9136c5cfcedebc5
AndresBasile <andresbasilealvarez@gmail.com>
2023-05-06 01:28:46 -0600

    falta funcionalidades de main y formshow activity y listo

--- app/src/main/java/com/example/basileejercicio1compumovil/FormShow.kt
+++ app/src/main/java/com/example/basileejercicio1compumovil/FormShow.kt
@@ -1,10 +1,19 @@
 package com.example.basileejercicio1compumovil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.annotation.SuppressLint
 import kotlin.concurrent.thread
+import com.example.basileejercicio1compumovil.databinding.ActivityFormShowBinding
 
 class FormShow : AppCompatActivity() {
+
+
+
+    private lateinit var binding: ActivityFormShowBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_show)
@@ -12,5 +21,58 @@ class FormShow : AppCompatActivity() {
 //        thread {
 //            Thread.sleep(5000)
 //        }
+
+
+        val bundle = intent.extras
+
+        if (bundle != null) {
+            val persona: Person? = bundle.getParcelable("persona")
+
+            if (persona != null) {
+                binding.tvNombrecompleto2.text = "${persona.nombre} ${persona.apellidos}"
+                binding.tvEdad2.text = resources.getQuantityString(R.string.edadR, persona.edad, persona.edad) --revosar
+                binding.tvSignozodiacal2.text = "${persona.signo}"
+                binding.tvSignozodiacalchino2.text = "${persona.signoChino}"
+                binding.tvCorreo2.text = "${persona.correo}"
+                binding.tvNumCuenta2.text = "${persona.numCuenta}"
+
+                val carreraImageResId = when (persona.carrera) {
+                    1 -> R.drawable.ingenieria_aeroespacial
+                    2 -> R.drawable.ingenieria_ambiental
+                    3 -> R.drawable.ingenieria_civil
+                    4 -> R.drawable.ingenieria_computacion
+                    5 -> R.drawable.ingenieria_electrica
+                    6 -> R.drawable.ingenieria_geofisica
+                    7 -> R.drawable.ingenieria_geologica
+                    8 -> R.drawable.ingenieria_geomatica
+                    9 -> R.drawable.ingenieria_industrial
+                    10 -> R.drawable.ingenieria_mecanica
+                    11 -> R.drawable.ingenieria_mecatronica
+                    12 -> R.drawable.ingenieria_minas
+                    13 -> R.drawable.ingenieria_petrolera
+                    14 -> R.drawable.ingenieria_biomedica
+                    15 -> R.drawable.ingenieria_telecomunicaciones
+                    else -> R.drawable.loading2
+                }
+
+                binding.ivCarrera.setImageResource(carreraImageResId)
+            }
+        }
+
+
     }
+
+
+
+
+
+
+
+    fun Regresar_click(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/com/example/basileejercicio1compumovil/MainActivity.kt
+++ app/src/main/java/com/example/basileejercicio1compumovil/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     var month = 0
     var year = 0
 
+    private fun checkMail(mail: CharSequence) = (!TextUtils.isEmpty(mail) && Patterns.EMAIL_ADDRESS.matcher(mail).matches())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,27 +36,174 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
 
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.ListaCarreras,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            binding.spinner.adapter = adapter
-//        }
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.ListaCarreras,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinner.adapter = adapter
+        }
 
+        binding.spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Log.d("LOGTAG", "El item seleccionado tiene la posición $position")
+            }
+            override fun onNothingSelected(parent: AdapterView<*>){
 
-
-
+            }
+        }
 //        val intent = Intent(this, FormShow::class.java)
 //        startActivity(intent)
 
     }
 
+    // con algo de ayuda de gpt
+    private fun validate(): Boolean {
+        return when {
+            binding.etNombre.text.isEmpty() -> {
+                binding.etNombre.error = resources.getString(R.string.ValorR)
+                Toast.makeText(this, resources.getString(R.string.NombreValidar), Toast.LENGTH_LONG).show()
+                false
+            }
+            binding.etApellidos.text.isEmpty() -> {
+                binding.etApellidos.error = resources.getString(R.string.ValorR)
+                Toast.makeText(this, resources.getString(R.string.ApellidosValidar), Toast.LENGTH_LONG).show()
+                false
+            }
+            binding.etDate.text.isEmpty() -> {
+                binding.etDate.error = resources.getString(R.string.ValorR)
+                Toast.makeText(this, resources.getString(R.string.FechaNacimientoValidar), Toast.LENGTH_LONG).show()
+                false
+            }
+            //Chat GPT
+            !checkMail(binding.etCorreo.text) -> {
+                binding.etCorreo.error = resources.getString(R.string.CorreoValidar)
+                Toast.makeText(this, resources.getString(R.string.CorreoValidar), Toast.LENGTH_LONG).show()
+                false
+            }
+            binding.etNumCuenta.text.isEmpty() || binding.etNumCuenta.text.length != 9 -> {
+                binding.etNumCuenta.error = resources.getString(R.string.ValorR)
+                Toast.makeText(this, resources.getString(R.string.NumCuentaValidar), Toast.LENGTH_LONG).show()
+                false
+            }
+            binding.spinner.selectedItemPosition == 0 -> {
+                Toast.makeText(this, resources.getString(R.string.CarreraValidar), Toast.LENGTH_LONG).show()
+                false
+            }
+            else -> true
+        }
+    }
 
 
 
+//
+//    //Chat GPT
+//    fun calcularSignoZodiacal(fecha: String): String? {
+//        val formatoFecha = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+//        val fechaNacimiento = formatoFecha.parse(fecha)
+//        val calendar = Calendar.getInstance().apply {
+//            time = fechaNacimiento!!
+//        }
+//        val dia = calendar.get(Calendar.DAY_OF_MONTH)
+//        val mes = calendar.get(Calendar.MONTH)
+//        val signos = resources.getStringArray(R.array.SignoZodiacal)
+//        val diasInicioSigno = intArrayOf(20, 19, 21, 20, 21, 21, 22, 23, 23, 23, 22, 22)
+//        var indiceSigno = mes
+//        if (dia < diasInicioSigno[mes]) {
+//            indiceSigno--
+//            if (indiceSigno < 0) {
+//                indiceSigno = 11
+//            }
+//        }
+//        return signos[indiceSigno]
+//    }
+//
+//    //chat GPT
+//    fun calcularSignoZodiacoChino(fecha: String): String? {
+//        val formatoFecha = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+//        val fechaNacimiento = formatoFecha.parse(fecha)
+//        val calendar = Calendar.getInstance().apply {
+//            time = fechaNacimiento!!
+//        }
+//        val year = calendar.get(Calendar.YEAR)
+//        val signos = resources.getStringArray(R.array.SignoChino)
+//        val cicloZodiacoChino = (year - 1900) % 12
+//        return signos[cicloZodiacoChino]
+//    }
+//
+//
+//    fun Calendario_click(view: View) {
+//        val Dialogfecha = DatePickerFragment{year, month, day -> printDate(year, month, day)}
+//
+//        Dialogfecha.show(supportFragmentManager, "DatePicker")
+//    }
+//
+//    private fun printDate(year: Int, month: Int, day: Int){
+//        binding.etDate.setText("$day/$month/$year")
+//    }
 
+//    //ChatGPT
+//    private fun calculaEdad(FechaNacimiento: String): Int{
+//        val fechaNac: Date = SimpleDateFormat("dd/MM/yyyy").parse(FechaNacimiento)
+//        val calendarFechaNac = Calendar.getInstance()
+//        calendarFechaNac.time = fechaNac
+//        val calendarHoy = Calendar.getInstance()
+//        var edad = calendarHoy.get(Calendar.YEAR) - calendarFechaNac.get(Calendar.YEAR)
+//        if (calendarHoy.get(Calendar.DAY_OF_YEAR) < calendarFechaNac.get(Calendar.DAY_OF_YEAR)) {
+//            edad--
+//        }
+//        return edad
+//    }
 
+//    fun clickContinuar(view: View) {
+//
+//        if (validarDatos()) {
+//            Toast.makeText(this, resources.getString(R.string.DatosCorrectos), Toast.LENGTH_LONG)
+//                .show()
+//            val intent = Intent(this, MuestraFormulario::class.java)
+//
+//            val bundle = Bundle()
+//            val persona = Persona(binding.etNombre.text.toString(),binding.etApellidos.text.toString(),calculaEdad(binding.etDate.text.toString()),calcularSignoZodiacal(binding.etDate.text.toString()),calcularSignoZodiacoChino(binding.etDate.text.toString()),binding.etCorreo.text.toString(),binding.etNumCuenta.text.toString().toInt(),binding.spinner.selectedItemPosition.toString().toInt())
+//
+//            bundle.putParcelable("persona", persona)
+//            intent.putExtras(bundle)
+//            startActivity(intent)
+//
+//        } else {
+//            Toast.makeText(this, resources.getString(R.string.DatosIncorrectos), Toast.LENGTH_LONG)
+//                .show()
+//        }
+//    }
+
+//    class DatePickerFragment(val listener:(year:Int, month:Int, day:Int) -> Unit): DialogFragment(), DatePickerDialog.OnDateSetListener{
+//
+//        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog{
+//            val c = Calendar.getInstance()
+//            var year = c.get(Calendar.YEAR)
+//            var month = c.get(Calendar.MONTH)
+//            var day = c.get(Calendar.DAY_OF_MONTH)
+//            //var dayOfYear = c.get(Calendar.DAY_OF_YEAR)
+//
+//            //val datePickerDialog = DatePickerDialog(requireActivity(),R.style.datePickerTheme,this, year, month, day)
+//            val datePickerDialog = DatePickerDialog(requireActivity(),this, year, month, day)
+//            val calendarMaxDate = Calendar.getInstance().apply {
+//                set(2010, 0, 1)}
+//            val maxDate = calendarMaxDate.timeInMillis
+//            //datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+//            datePickerDialog.datePicker.maxDate = maxDate
+//            val calendarMinDate = Calendar.getInstance().apply {
+//                set(1900, 0, 1)}
+//            val minDate = calendarMinDate.timeInMillis
+//            datePickerDialog.datePicker.minDate = minDate
+//
+//            return datePickerDialog
+//
+//        }
+//
+//        override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+//            listener(year,month+1,day)
+//        }
+//    }
 
 }
