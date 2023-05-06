ef7061f426fb4eb953c42a428db37efa8e207e50
AndresBasile <andresbasilealvarez@gmail.com>
2023-05-05 23:55:10 -0600

    icono, main act xml, showform xml

--- app/src/main/java/com/example/basileejercicio1compumovil/FormShow.kt
+++ app/src/main/java/com/example/basileejercicio1compumovil/FormShow.kt
@@ -2,10 +2,15 @@ package com.example.basileejercicio1compumovil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.thread
 
 class FormShow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_show)
+        supportActionBar?.hide()
+//        thread {
+//            Thread.sleep(5000)
+//        }
     }
 }
--- app/src/main/java/com/example/basileejercicio1compumovil/MainActivity.kt
+++ app/src/main/java/com/example/basileejercicio1compumovil/MainActivity.kt
@@ -1,17 +1,61 @@
 package com.example.basileejercicio1compumovil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basileejercicio1compumovil.databinding.ActivityMainBinding
+import android.app.Dialog
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
 
+    private lateinit var binding: ActivityMainBinding
 
+    var accNumber = 0
+    var day = 0
+    var month = 0
+    var year = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.ListaCarreras,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            binding.spinner.adapter = adapter
+//        }
+
+
+
+
+//        val intent = Intent(this, FormShow::class.java)
+//        startActivity(intent)
+
     }
+
+
+
+
+
+
+
 }
